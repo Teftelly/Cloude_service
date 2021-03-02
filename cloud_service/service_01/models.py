5691514af6f7f361ea84fd5283c3f0e467ee3fd6
kyzmin <kuzmin@cmsplanet.ru>
2021-03-02 14:22:50 +0300

    update models

--- cloud_service/service_01/models.py
+++ cloud_service/service_01/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class User_History(models)
+	history_file = models.FileField(
+        'файл исстории',
+        blank=True,
+        upload_to='history_files'
+        )
