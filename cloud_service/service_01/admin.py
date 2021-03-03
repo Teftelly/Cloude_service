c0933493de3729f7e8e8cd9b784057cdf241a0bc
Alina <38279777+Teftelly@users.noreply.github.com>
2021-03-03 22:55:19 +0300

    02
    Изменеия в модели. Попытки загрузить картинку через админку. Вылетает ValueError at /admin/service_01/user_history/add/
    Invalid format string при попытке сохранить

--- cloud_service/service_01/admin.py
+++ cloud_service/service_01/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User_History, Test_Us_HF
+
+admin.site.register(User_History)
+
+admin.site.register(Test_Us_HF)
--- cloud_service/service_01/models.py
+++ cloud_service/service_01/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
-class User_History(models)
-	history_file = models.FileField(
-        'файл исстории',
-        blank=True,
-        upload_to='history_files'
-        )
+
+class User_History(models.Model):
+	User = models.ForeignKey(
+		User,
+		on_delete = models.CASCADE
+	)
+	History_File = models.ImageField(
+		'Файл истории пользователя',
+
+		# If True, the field is allowed to be blank. Default is False.
+
+		blank = True,
+
+		# upload_to - Этот атрибут обеспечивает способ установки каталога 
+		# загрузки и имени файла. 
+		# файл будет сохранен в MEDIA_ROOT / History_Files / YYYY/MM/DD
+
+		upload_to = 'History_Files/% Y/% m/% d/'
+	)
+
+class Test_Us_HF(models.Model):
+	text = models.CharField(max_length = 120)
